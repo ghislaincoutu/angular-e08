2f8c83e10d1053aec23ec84e8d1d79ec57826321
ghislain.coutu <gc8395@gmail.com>
2022-12-26 13:19:49 -0500

    Exercice Angular

--- related_files/scripts/generate_code_preview.sh
+++ related_files/scripts/generate_code_preview.sh
@@ -6,9 +6,9 @@ echo "Copie du code HTML et TypeScript dans les fichiers code-preview"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
-so=/home/Exercices/angular-e08/src/app
+so=/home/Exercises/angular-e08/src/app
 # Destination (de)
-de=/home/Exercices/angular-e08/src/assets/code-preview/
+de=/home/Exercises/angular-e08/src/assets/code-preview/
 
 cp $so/t02/t02.component.html $de/t02/
 mv $de/t02/t02.component.html $de/t02/html.txt
--- related_files/scripts/publish_application.sh
+++ related_files/scripts/publish_application.sh
@@ -6,7 +6,7 @@ echo "Publication de l'applications sur le serveur Web local"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
-so=/home/Exercices/angular-e08/dist/angular-e08
+so=/home/Exercises/angular-e08/dist/angular-e08
 # Destination (de)
 de=/var/www/html/r002/angular-e08
 
