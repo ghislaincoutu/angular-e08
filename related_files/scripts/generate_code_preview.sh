c7f3e5c710324aa7bf653a0d7bffa2548d5875c0
ghislain.coutu <gc8395@gmail.com>
2023-07-19 06:01:22 -0400

    Exercice Angular

--- related_files/scripts/generate_code_preview.sh
+++ related_files/scripts/generate_code_preview.sh
@@ -21,10 +21,10 @@ mv $de/t01/t01.component.html $de/t01/html.txt
 cp $so/t01/t01.component.ts $de/t01/
 mv $de/t01/t01.component.ts $de/t01/typescript.txt
 
-cp $so/t03/t03.component.html $de/t03/
-mv $de/t03/t03.component.html $de/t03/html.txt
-cp $so/t03/t03.component.ts $de/t03/
-mv $de/t03/t03.component.ts $de/t03/typescript.txt
+cp $so/t02/t02.component.html $de/t02/
+mv $de/t02/t02.component.html $de/t02/html.txt
+cp $so/t02/t02.component.ts $de/t02/
+mv $de/t02/t02.component.ts $de/t02/typescript.txt
 
 cp $so/t03/t03.component.html $de/t03/
 mv $de/t03/t03.component.html $de/t03/html.txt
